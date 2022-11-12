fba7edfb25a76fccc3b007f19932fb750ecf80e4
liampatterson02@gmail.com <liampatterson02@gmail.com>
2022-11-12 18:53:25 -0500

    basic curve operations

--- elliptic-curve-ElGamal.py
+++ elliptic-curve-ElGamal.py
@@ -1,93 +1,75 @@
-# Python program to illustrate ElGamal encryption
+INF_POINT = None
+class EllipticCurve:
 
-import random
-from math import pow
+	def __init__(self):
 
-a = random.randint(2, 10)
+		#secp160r1 parameters
+		self.a = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7FFFFFFC
+		self.b = 0x1C97BEFC54BD7A8B65ACF89F81D4D4ADC565FA45
+		self.p = 2**160 - 2**31 - 1
 
-def gcd(a, b):
-	if a < b:
-		return gcd(b, a)
-	elif a % b == 0:
-		return b
-	else:
-		return gcd(b, a % b)
-
-# Generating large random numbers
-def gen_key(q):
-
-	key = random.randint(pow(10, 20), q)
-	while gcd(q, key) != 1:
-		key = random.randint(pow(10, 20), q)
-
-	return key
-
-# Modular exponentiation
-def power(a, b, c):
-	x = 1
-	y = a
-
-	while b > 0:
-		if b % 2 != 0:
-			x = (x * y) % c
-		y = (y * y) % c
-		b = int(b / 2)
-
-	return x % c
-
-# Asymmetric encryption
-def encrypt(msg, q, h, g):
-
-	en_msg = []
-
-	k = gen_key(q)# Private key for sender
-	s = power(h, k, q)
-	p = power(g, k, q)
-	
-	for i in range(0, len(msg)):
-		en_msg.append(msg[i])
-
-	print("g^k used : ", p)
-	print("g^ak used : ", s)
-	for i in range(0, len(en_msg)):
-		en_msg[i] = s * ord(en_msg[i])
-
-	return en_msg, p
-
-def decrypt(en_msg, p, key, q):
-
-	dr_msg = []
-	h = power(p, key, q)
-	for i in range(0, len(en_msg)):
-		dr_msg.append(chr(int(en_msg[i]/h)))
+		self.gx = 0x4A96B5688EF573284664698968C38BB913CBFC82
+		self.gy = 0x23A628553168947D59DCC912042351377AC5FB32
+		self.g = (self.gx, self.gy)
 		
-	return dr_msg
+		self.n = 0x00000000000000000001F4C8F927AED3CA752257
 
-# Driver code
-def main():
+		self.privateKey = 0x1053CDE42C14D696E67687561517533BF3F83345
 
-    
-    d = 2
+		self.points = []
+		self.definePoints()
+	
+	
+	def modinv(self, a, n):
+		lm, hm = 1, 0 
+		low, high = a%n, n
 
-    msg = 'I am an undergraduate student at queen’s university.'
-    print("Original Message :", msg)
+		while low > 1:
+			r = high/low
 
-    q = random.randint(pow(10, 20), pow(10, 50))
-    g = random.randint(2, q)
+			nm = hm-lm*r
+			temp = high-low*r
 
-    key = gen_key(q)# Private key for receiver
-    h = power(g, key, q)
-    print("g used : ", g)
-    print("g^a used : ", h)
+			lm, low, hm, high = nm, temp, lm, low
+		return lm % n
 
-    en_msg, p = encrypt(msg, q, h, g)
-    print("Encrypted Message :", en_msg)
+	def add(self, x1, x2):
 
+		num = (x2[1] - x1[1])
+		denom = self.modinv(x2[0]-x1[0], self.p)
+		lam =  (num*denom) % self.p
 
-    # dr_msg = decrypt(en_msg, p, key, q)
-    # dmsg = ''.join(dr_msg)
-    # print("Decrypted Message :", dmsg)  
+		x = (lam*lam-x1[0]-x2[0]) % self.p
+		y = (lam*(x1[0]-x) -x1[1]) % self.p
 
+		return (x,y)
 
-if __name__ == '__main__':
-	main()
+	def double(self, x1):
+
+		num = (3*x1[0]*x1[0] + self.a)
+
+		denom = self.modinv(2*x1[1], self.p)
+		lam =  (num*denom) % self.p
+
+		x = (lam*lam- 2*x1[0]) % self.p
+		y = (lam*(x1[0]-x) -x1[1]) % self.p
+
+		return (x,y)
+
+	def multiply(self, g, scalar):
+
+		if scalar == 0 or scalar > self.n:
+			raise Exception("Invalid scalar")
+		
+		scalarBin = str(bin(scalar))[2:]
+		q = g
+
+		for i in range(1, len(scalarBin)):
+			q = self.double(q)
+
+			if scalarBin[i] == "1":
+				q = self.add(q, g)
+		return q
+
+ec = EllipticCurve()
+
